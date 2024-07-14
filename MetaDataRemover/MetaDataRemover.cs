d1a8426bdd27b751c4ac1f0502c3685c36ed8ce0
NaveenKumarWishlist <naveen@enjoywishlist.com>
2024-07-14 13:11:21 +0530

    Add project files.

---
+++ MetaDataRemover/MetaDataRemover.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using SixLabors.ImageSharp;
+
+namespace MetaData.Remover.Classes;
+
+public class MetaDataRemover
+{
+    public static Stream RemoveMetadataFromStream(Stream inStream, Stream outStream)
+    {
+        if (inStream == null)
+        {
+            throw new ArgumentNullException("inStream");
+        }
+
+        if (outStream == null)
+        {
+            throw new NullReferenceException("outStream");
+        }
+
+        try
+        {
+            using (Image image = Image.Load(inStream))
+            {
+                image.Metadata.ExifProfile = null;
+                image.Metadata.IptcProfile = null;
+                image.Metadata.XmpProfile = null;
+                image.Save(outStream, image.Metadata.DecodedImageFormat);
+            }
+
+            return outStream;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public static void RemoveMetadataUsingPath(string inputPath, string outputPath)
+    {
+        if (string.IsNullOrWhiteSpace(inputPath))
+        {
+            throw new ArgumentNullException("inputPath");
+        }
+
+        if (string.IsNullOrWhiteSpace(outputPath))
+        {
+            throw new ArgumentNullException("outputPath");
+        }
+
+        try
+        {
+            using Image image = Image.Load(inputPath);
+            image.Metadata.ExifProfile = null;
+            image.Metadata.IptcProfile = null;
+            image.Metadata.XmpProfile = null;
+            image.Save(outputPath);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+}
